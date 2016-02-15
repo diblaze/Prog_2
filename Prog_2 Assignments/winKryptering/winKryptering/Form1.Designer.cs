73b91859f0a6dc6b05d1d93798443893fff7fe6c
diblaze <DeniCevap@gmail.com>
2016-02-15 21:39:00 +0100

    Finished "Kryptering"

--- Prog_2 Assignments/winKryptering/winKryptering/Form1.Designer.cs
+++ Prog_2 Assignments/winKryptering/winKryptering/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.lblFile = new System.Windows.Forms.Label();
-            this.tbEncrypt = new System.Windows.Forms.TextBox();
+            this.tbTextToEncrypt = new System.Windows.Forms.TextBox();
             this.lblEncryptText = new System.Windows.Forms.Label();
             this.lblDecryptedText = new System.Windows.Forms.Label();
-            this.tbDecrypt = new System.Windows.Forms.TextBox();
+            this.tbEncryptedText = new System.Windows.Forms.TextBox();
             this.btnOpen = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnQuit = new System.Windows.Forms.Button();
@@ -46,51 +46,57 @@
             // lblFile
             // 
             this.lblFile.AutoSize = true;
-            this.lblFile.Location = new System.Drawing.Point(34, 73);
+            this.lblFile.Location = new System.Drawing.Point(23, 47);
+            this.lblFile.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFile.Name = "lblFile";
-            this.lblFile.Size = new System.Drawing.Size(51, 20);
+            this.lblFile.Size = new System.Drawing.Size(35, 13);
             this.lblFile.TabIndex = 0;
             this.lblFile.Text = "label1";
             // 
-            // tbEncrypt
+            // tbTextToEncrypt
             // 
-            this.tbEncrypt.Location = new System.Drawing.Point(34, 233);
-            this.tbEncrypt.Multiline = true;
-            this.tbEncrypt.Name = "tbEncrypt";
-            this.tbEncrypt.Size = new System.Drawing.Size(495, 636);
-            this.tbEncrypt.TabIndex = 1;
+            this.tbTextToEncrypt.Location = new System.Drawing.Point(23, 151);
+            this.tbTextToEncrypt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbTextToEncrypt.Multiline = true;
+            this.tbTextToEncrypt.Name = "tbTextToEncrypt";
+            this.tbTextToEncrypt.Size = new System.Drawing.Size(331, 415);
+            this.tbTextToEncrypt.TabIndex = 1;
             // 
             // lblEncryptText
             // 
             this.lblEncryptText.AutoSize = true;
-            this.lblEncryptText.Location = new System.Drawing.Point(34, 193);
+            this.lblEncryptText.Location = new System.Drawing.Point(23, 125);
+            this.lblEncryptText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEncryptText.Name = "lblEncryptText";
-            this.lblEncryptText.Size = new System.Drawing.Size(51, 20);
+            this.lblEncryptText.Size = new System.Drawing.Size(35, 13);
             this.lblEncryptText.TabIndex = 2;
             this.lblEncryptText.Text = "label2";
             // 
             // lblDecryptedText
             // 
             this.lblDecryptedText.AutoSize = true;
-            this.lblDecryptedText.Location = new System.Drawing.Point(807, 193);
+            this.lblDecryptedText.Location = new System.Drawing.Point(538, 125);
+            this.lblDecryptedText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDecryptedText.Name = "lblDecryptedText";
-            this.lblDecryptedText.Size = new System.Drawing.Size(51, 20);
+            this.lblDecryptedText.Size = new System.Drawing.Size(35, 13);
             this.lblDecryptedText.TabIndex = 4;
             this.lblDecryptedText.Text = "label3";
             // 
-            // tbDecrypt
+            // tbEncryptedText
             // 
-            this.tbDecrypt.Location = new System.Drawing.Point(807, 233);
-            this.tbDecrypt.Multiline = true;
-            this.tbDecrypt.Name = "tbDecrypt";
-            this.tbDecrypt.Size = new System.Drawing.Size(495, 636);
-            this.tbDecrypt.TabIndex = 3;
+            this.tbEncryptedText.Location = new System.Drawing.Point(538, 151);
+            this.tbEncryptedText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbEncryptedText.Multiline = true;
+            this.tbEncryptedText.Name = "tbEncryptedText";
+            this.tbEncryptedText.Size = new System.Drawing.Size(331, 415);
+            this.tbEncryptedText.TabIndex = 3;
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(409, 178);
+            this.btnOpen.Location = new System.Drawing.Point(273, 116);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(120, 49);
+            this.btnOpen.Size = new System.Drawing.Size(80, 32);
             this.btnOpen.TabIndex = 5;
             this.btnOpen.Text = "button1";
             this.btnOpen.UseVisualStyleBackColor = true;
@@ -98,18 +104,21 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(1182, 178);
+            this.btnSave.Location = new System.Drawing.Point(788, 116);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(120, 49);
+            this.btnSave.Size = new System.Drawing.Size(80, 32);
             this.btnSave.TabIndex = 6;
             this.btnSave.Text = "button2";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnQuit
             // 
-            this.btnQuit.Location = new System.Drawing.Point(1182, 893);
+            this.btnQuit.Location = new System.Drawing.Point(788, 580);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnQuit.Name = "btnQuit";
-            this.btnQuit.Size = new System.Drawing.Size(120, 49);
+            this.btnQuit.Size = new System.Drawing.Size(80, 32);
             this.btnQuit.TabIndex = 7;
             this.btnQuit.Text = "button3";
             this.btnQuit.UseVisualStyleBackColor = true;
@@ -117,9 +126,10 @@
             // 
             // btnRovar
             // 
-            this.btnRovar.Location = new System.Drawing.Point(613, 275);
+            this.btnRovar.Location = new System.Drawing.Point(409, 179);
+            this.btnRovar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRovar.Name = "btnRovar";
-            this.btnRovar.Size = new System.Drawing.Size(120, 86);
+            this.btnRovar.Size = new System.Drawing.Size(80, 56);
             this.btnRovar.TabIndex = 8;
             this.btnRovar.Text = "button4";
             this.btnRovar.UseVisualStyleBackColor = true;
@@ -127,35 +137,40 @@
             // 
             // btnILang
             // 
-            this.btnILang.Location = new System.Drawing.Point(613, 367);
+            this.btnILang.Location = new System.Drawing.Point(409, 239);
+            this.btnILang.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnILang.Name = "btnILang";
-            this.btnILang.Size = new System.Drawing.Size(120, 86);
+            this.btnILang.Size = new System.Drawing.Size(80, 56);
             this.btnILang.TabIndex = 9;
             this.btnILang.Text = "button5";
             this.btnILang.UseVisualStyleBackColor = true;
+            this.btnILang.Click += new System.EventHandler(this.btnILang_Click);
             // 
             // btnFikon
             // 
             this.btnFikon.Enabled = false;
-            this.btnFikon.Location = new System.Drawing.Point(613, 459);
+            this.btnFikon.Location = new System.Drawing.Point(409, 298);
+            this.btnFikon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnFikon.Name = "btnFikon";
-            this.btnFikon.Size = new System.Drawing.Size(120, 86);
+            this.btnFikon.Size = new System.Drawing.Size(80, 56);
             this.btnFikon.TabIndex = 10;
             this.btnFikon.Text = "button6";
             this.btnFikon.UseVisualStyleBackColor = true;
             // 
             // tbFileLocation
             // 
-            this.tbFileLocation.Location = new System.Drawing.Point(92, 73);
+            this.tbFileLocation.Location = new System.Drawing.Point(61, 47);
+            this.tbFileLocation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbFileLocation.Name = "tbFileLocation";
-            this.tbFileLocation.Size = new System.Drawing.Size(355, 26);
+            this.tbFileLocation.Size = new System.Drawing.Size(238, 20);
             this.tbFileLocation.TabIndex = 11;
             // 
             // btnOpenFileDialog
             // 
-            this.btnOpenFileDialog.Location = new System.Drawing.Point(453, 71);
+            this.btnOpenFileDialog.Location = new System.Drawing.Point(302, 46);
+            this.btnOpenFileDialog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOpenFileDialog.Name = "btnOpenFileDialog";
-            this.btnOpenFileDialog.Size = new System.Drawing.Size(75, 26);
+            this.btnOpenFileDialog.Size = new System.Drawing.Size(50, 17);
             this.btnOpenFileDialog.TabIndex = 12;
             this.btnOpenFileDialog.Text = "button1";
             this.btnOpenFileDialog.UseVisualStyleBackColor = true;
@@ -163,9 +178,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1330, 954);
+            this.ClientSize = new System.Drawing.Size(887, 620);
             this.Controls.Add(this.btnOpenFileDialog);
             this.Controls.Add(this.tbFileLocation);
             this.Controls.Add(this.btnFikon);
@@ -175,10 +190,11 @@
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.lblDecryptedText);
-            this.Controls.Add(this.tbDecrypt);
+            this.Controls.Add(this.tbEncryptedText);
             this.Controls.Add(this.lblEncryptText);
-            this.Controls.Add(this.tbEncrypt);
+            this.Controls.Add(this.tbTextToEncrypt);
             this.Controls.Add(this.lblFile);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Kryptering";
             this.ResumeLayout(false);
@@ -189,10 +205,10 @@
         #endregion
 
         private System.Windows.Forms.Label lblFile;
-        private System.Windows.Forms.TextBox tbEncrypt;
+        private System.Windows.Forms.TextBox tbTextToEncrypt;
         private System.Windows.Forms.Label lblEncryptText;
         private System.Windows.Forms.Label lblDecryptedText;
-        private System.Windows.Forms.TextBox tbDecrypt;
+        private System.Windows.Forms.TextBox tbEncryptedText;
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnQuit;
--- Prog_2 Assignments/winKryptering/winKryptering/Form1.cs
+++ Prog_2 Assignments/winKryptering/winKryptering/Form1.cs
@@ -1,19 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace winKryptering
 {
     public partial class Form1 : Form
     {
-        public string FileName { get; private set; }
+        private readonly char[] _vowelsToCheckFor =
+        {
+            'a',
+            'A',
+            'o',
+            'O',
+            'u',
+            'U',
+            'å',
+            'Å',
+            'e',
+            'E',
+            'i',
+            'I',
+            'y',
+            'Y',
+            'ä',
+            'Ä',
+            'ö',
+            'Ö'
+        };
 
         public Form1()
         {
@@ -23,7 +38,6 @@ namespace winKryptering
             FormBorderStyle = FormBorderStyle.Fixed3D;
             btnOpenFileDialog.Text = @"...";
             btnOpenFileDialog.Size = new Size(40, 22);
-            
 
             //Label texts
             lblFile.Text = @"File:";
@@ -37,69 +51,55 @@ namespace winKryptering
             btnRovar.Text = @"Rövarspråket";
             btnILang.Text = @"I-språket";
             btnFikon.Text = @"Fikonspråket";
+        }
 
+        public string FileName
+        {
+            get;
+            private set;
         }
 
         private void btnOpenFileDialog_Click(object sender, EventArgs e)
         {
-            OpenFileDialog dlg = new OpenFileDialog();
+            var openFileDialog = new OpenFileDialog();
 
-            if (dlg.ShowDialog() != DialogResult.OK)
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
                 return;
+            }
 
-            FileName = dlg.FileName;
+            FileName = openFileDialog.FileName;
             tbFileLocation.Text = FileName;
         }
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
             if (FileName == null)
+            {
                 return;
+            }
 
-            StreamReader streamReader = new StreamReader(FileName);
-            tbEncrypt.Text = streamReader.ReadToEnd();
-
+            var streamReader = new StreamReader(FileName);
+            tbTextToEncrypt.Text = streamReader.ReadToEnd();
         }
 
         private void btnRovar_Click(object sender, EventArgs e)
         {
-
-            if (tbEncrypt.Text == string.Empty)
-                return;
-
-            string textToEncrypt = tbEncrypt.Text;
-            var convertedText = "";
-
-            char[] charsToCheckFor =
+            if (tbTextToEncrypt.Text == string.Empty)
             {
-                'a',
-                'A',
-                'o',
-                'O',
-                'u',
-                'U',
-                'å',
-                'Å',
-                'e',
-                'E',
-                'i',
-                'I',
-                'y',
-                'Y',
-                'ä',
-                'Ä',
-                'ö',
-                'Ö'
-            };
+                return;
+            }
+
+            string textToEncrypt = tbTextToEncrypt.Text;
+            string convertedText = "";
 
             foreach (char t in textToEncrypt)
             {
                 if (char.IsWhiteSpace(t))
                 {
-                    continue;
-                    
+                    convertedText += t;
                 }
-                if (charsToCheckFor.Contains(t))
+                else if (_vowelsToCheckFor.Contains(t))
                 {
                     convertedText += t;
                 }
@@ -107,16 +107,59 @@ namespace winKryptering
                 {
                     convertedText += t;
                     convertedText += 'o';
-                    convertedText += t;
+                    convertedText += t.ToString().ToLower();
                 }
             }
 
-            tbDecrypt.Text = convertedText;
+            tbEncryptedText.Text = convertedText;
         }
 
         private void btnQuit_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            using (var sw = new StreamWriter(saveFileDialog.FileName))
+            {
+                sw.WriteLine(tbEncryptedText.Text);
+            }
+        }
+
+        private void btnILang_Click(object sender, EventArgs e)
+        {
+            if (tbTextToEncrypt.Text == string.Empty)
+            {
+                return;
+            }
+
+            string textToEncrypt = tbTextToEncrypt.Text;
+            string convertedText = "";
+
+            foreach (char t in textToEncrypt)
+            {
+                if (char.IsWhiteSpace(t))
+                {
+                    convertedText += t;
+                }
+                else if (_vowelsToCheckFor.Contains(t))
+                {
+                    convertedText += 'i';
+                }
+                else
+                {
+                    convertedText += t;
+                }
+            }
+
+            tbEncryptedText.Text = convertedText;
         }
     }
 }
